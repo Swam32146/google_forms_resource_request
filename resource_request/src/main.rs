73f475c721f8ea11aff1a30f45297de54ca1193a
Swam32146 <prodigybatman32146@gmail.com>
2025-04-08 14:07:40 -0400

    Update main.rs

--- resource_request/src/main.rs
+++ resource_request/src/main.rs
@@ -1,6 +1,5 @@
 use google_sheets4::yup_oauth2::ServiceAccountAuthenticator;
 use google_sheets4::yup_oauth2::ServiceAccountKey;
-use serde_json::Value;
 use std::error::Error;
 use std::env;
 use std::fs;
@@ -9,8 +8,11 @@ use std::path::Path;
 mod get_json;
 
 
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+// This was a test function to do sometin
+/* 
     let json_str: &str = r#"
     {
         "client_id": "hello world",
@@ -23,6 +25,28 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     println!("{}", String::from(client_id));
     println!("ZimZimzalabim");
+*/
+    dotenvy::dotenv()?;
+
+    let json_key_path = match env::var("GOOGLE_SERVICE_ACCOUNT") {
+        Ok(json_file) => json_file,
+        Err(_) => {
+            eprintln!("Error: FILE AINT FOUND ion that env crodie");
+            std::process::exit(1);
+        }
+    };
+
+    println!("{}", &json_key_path);
+
+    let google_json_file = match fs::read_to_string(&json_key_path) {
+        Ok(opened_string) => opened_string,
+        Err(__) => {
+            eprintln!("Error this how aint got nothin");
+            std::process::exit(1);
+        }
+    };
+
+    println!("{}", String::to_string(&google_json_file));
 
     Ok(())
 
